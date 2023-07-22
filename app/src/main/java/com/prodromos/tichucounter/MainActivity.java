60d260b43c5744bf478d1011b04220a17ebae14d
pravramidis <pravramidis@gmail.com>
2023-07-22 10:47:14 +0300

    Create the basic tabs and the navbar

--- app/src/main/java/com/prodromos/tichucounter/MainActivity.java
+++ app/src/main/java/com/prodromos/tichucounter/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.prodromos.tichucounter.databinding.ActivityMainBinding;
 
@@ -22,16 +24,33 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        binding.navView.setOnItemSelectedListener(item -> {
+
+
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.navigation_home) {
+                replaceFragment(new HomeFragment());
+            }
+            else if (itemId == R.id.navigation_game) {
+                replaceFragment(new GameFragment());
+            }
+            else if (itemId == R.id.navigation_profiles) {
+                replaceFragment(new ProfilesFragment());
+            }
+
+            return true;
+        } );
+    }
+
+    private void replaceFragment(Fragment fragment ) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 
 }
