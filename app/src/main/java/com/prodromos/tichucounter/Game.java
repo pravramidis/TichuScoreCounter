357eb77195eb1dda8ca7ada9d63bb0eb50df4d81
pravramidis <pravramidis@gmail.com>
2023-08-13 12:21:48 +0300

    Add basic elements to the game screen

---
+++ app/src/main/java/com/prodromos/tichucounter/Game.java
@@ -0,0 +1,16 @@
+package com.prodromos.tichucounter;
+import java.util.ArrayList;
+
+public class Game {
+
+    String team1Player1 = "Placeholder";
+    String team1Player2 = "Placeholder";
+    String team2Player1 = "Placeholder";
+    String team2Player2 = "Placeholder";
+
+    int pointsTeam1 = 0;
+    int pointsTeam2 = 0;
+
+    ArrayList<Match> matchHistory;
+
+}
---
+++ app/src/main/java/com/prodromos/tichucounter/Match.java
@@ -0,0 +1,11 @@
+package com.prodromos.tichucounter;
+
+public class Match {
+    int titchuTeam1;
+    int grandTeam1;
+    int titchuTeam2;
+    int grandTeam2;
+    int pointsTeam1;
+    int pointsTeam2;
+
+}
